709fbb0a54d74bd7ce7bb29bd24ee6c9cc10ae8f
Galina Bilonog <galina.bilonog@ab-soft.net>
2022-02-23 23:08:12 +0200

    hw2

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
 const { info, warn, error } = require('./utils/logger');
+const { seek, eventEmitter } = require('./utils/FileSeeker');
+const { argv } = require('process');
 
 console.log('hello, world)');
 info('green info message');
 warn('yellow warn message');
 error('red error message');
+
+eventEmitter.on('success', (target,dirPath) => {
+    info(`file ${target} is present in folder ${dirPath}`);
+})
+eventEmitter.on('fail', (target,dirPath) => {
+    warn(`file ${target} is not present in folder ${dirPath}`);
+})
+
+seek('package-lock.json', __dirname);//success
+seek('package-lock.jsn', __dirname);//fail
+argv.forEach((val, index) => {
+    console.log(`${index}: ${val}`);
+  });
+seek(argv[2], argv[3]);//success
+seek(argv[2], argv[1]);//error
---
+++ utils/FileSeeker.js
@@ -0,0 +1,26 @@
+const { readdir } = require('fs');
+const path = require('path');
+const { info, warn, error } = require('./logger');
+const EventEmitter = require('events');
+const eventEmitter = new EventEmitter();
+
+
+function seek(target = '', dirPath = '') { 
+    readdir(path.resolve(dirPath), (err, files)=>{
+        if(err) {
+            error(err);
+        } else {
+            info(`files: ${files} are in folder ${dirPath}`);
+            if(files.indexOf(target) !== -1){
+                eventEmitter.emit('success', target, dirPath);
+            } else {
+                eventEmitter.emit('fail', target, dirPath);
+            }
+        }
+    })
+
+
+}
+
+module.exports = { seek, eventEmitter };
+
