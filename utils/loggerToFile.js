0bc0ac5fd9109a6686466fd12407f9af2bc3a1f7
Galina Bilonog <galina.bilonog@ab-soft.net>
2022-04-03 10:00:55 +0300

    hw4

--- utils/loggerToFile.js
+++ utils/loggerToFile.js
@@ -1,12 +1,11 @@
-const { appendFile } = require('fs');
+const { appendFile, createWriteStream } = require('fs');
 
 const date = new Date().toUTCString();
 
 function writeToLog(...args){
     const logMessage = '\n' + date + '[ ' + args[0] +' ] '+ (args[1]||'');
-    appendFile('events.log', logMessage, 'utf8', (err) => {
-        if (err) throw err;
-      }); 
+    let writer = createWriteStream('./events.log', {encoding: 'utf8', flags: 'a+'});
+    writer.write(logMessage);
 }
 
 module.exports = { writeToLog };
